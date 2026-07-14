77cb5b562079f9871d41c8a4de841ada36db2de8
benjaminjofre <benj.jofre@duocuc.cl>
2026-07-13 22:10:37 -0400

    Add files via upload
    para eureka

---
+++ conexion_de_hospital/src/test/java/com/example/conexion_de_hospital/ConexionDeHospitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.conexion_de_hospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConexionDeHospitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

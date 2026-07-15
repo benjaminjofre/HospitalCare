d5fa0d815edb242951aea7e7a8aa46d273e3dbd3
benjaminjofre <benj.jofre@duocuc.cl>
2026-07-14 21:07:37 -0400

    Delete src directory

--- src/main/java/com/hospital_vm_vl/hospital_vm/Cita/exception/ResourceNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.hospital_vm_vl.hospital_vm.Cita.exception;
-
-public class ResourceNotFoundException extends RuntimeException{
-    public ResourceNotFoundException(String mensaje){
-
-        super(mensaje);
-
-    }
-}
--- src/main/java/com/hospital_vm_vl/hospital_vm/Examenes/repository/ExamenesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hospital_vm_vl.hospital_vm.Examenes.repository;
-
-import com.hospital_vm_vl.hospital_vm.Examenes.model.Examenes;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ExamenesRepository extends JpaRepository<Examenes, Long> {
-}

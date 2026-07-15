1059565ac5d7b773aa2b5182f60b0022dab9f044
benjaminjofre <benj.jofre@duocuc.cl>
2026-07-14 21:00:39 -0400

    Delete target directory

--- target/classes/db/migration/V1_Crear_Tabla.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE Pacientes (
-                           id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                           nombre VARCHAR(100) NOT NULL,
-                           rut VARCHAR(12) NOT NULL
-);
-
-INSERT INTO Pacientes (nombre, rut) VALUES ('Juan Pérez', '12.345.678-9');
